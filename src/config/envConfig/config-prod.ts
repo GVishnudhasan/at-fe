d15596891eb4591b229869193107831e2cde2538
GVishnudhasan <gvishnud10@gmail.com>
2025-10-26 01:14:47 +0530

    chore(config): update environment configuration

--- src/config/envConfig/config-prod.ts
+++ src/config/envConfig/config-prod.ts
@@ -1,6 +1,8 @@
 const prodConfig = {
-  baseApiUrl: 'http://127.0.0.1:10000',
-  baseUrlServerApi: 'http://51.20.56.71/api-gateway/at-data-service/v1.0.0',
+  baseApiUrl: 'http://localhost:3000',
+  baseUrlServerApi: 'https://admin-gb.onrender.com/at-data-service/v1.0.0',
+  // baseUrlServerApi: 'http://localhost:4000/at-data-service/v1.0.0',
+
 };
 
 export default prodConfig;
